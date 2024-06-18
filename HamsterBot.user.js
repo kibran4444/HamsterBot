3df0b4280beb6f622ad0dfa0441b6dcf40b2d6f1
kibran4444 <83709771+kibran4444@users.noreply.github.com>
2024-06-19 00:11:57 +0800

    Update HamsterBot.user.js

--- HamsterBot.user.js
+++ HamsterBot.user.js
@@ -161,7 +161,11 @@
 
 
 
-         
+         //   const buttonBoost = document.querySelector('.boost-item');
+       // if (buttonBooster) {
+       // buttonBooster.onclick();
+           // console.log(`${logPrefix}Нажата кнопка ${buttonBooster[0]}`, styles.success);
+          // }
 
 
             // Установка задержки перед следующей проверкой энергии
