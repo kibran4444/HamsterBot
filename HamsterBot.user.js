fba744d913fe73cfd32247bac13e0d491c439813
kibran4444 <83709771+kibran4444@users.noreply.github.com>
2024-06-19 00:00:27 +0800

    Update HamsterBot.user.js

--- HamsterBot.user.js
+++ HamsterBot.user.js
@@ -2,7 +2,7 @@
 // @name         Hamster Bot
 // @namespace    Violentmonkey Scripts
 // @match        *://*.hamsterkombat.io/*
-// @version      1.0
+// @version      1.1
 // @description  Тапалка для Хомяка
 // @grant        none
 // @icon
